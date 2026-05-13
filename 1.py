4fcb2eb477623c580c5e7456bdb7a84c31e64801
吴 波涛 <wubotao@datamatrixai.com>
2026-05-13 15:51:57 +0800

    '第十次提交'

--- 1.py
+++ 1.py
@@ -9,13 +9,9 @@ for i in range(100):
 
 print("*********")
 for i in range(100):
-<<<<<<< HEAD
     print(a*b)
 
 
 print("*********")
 for i in range(100):
     print(a==b)
-=======
-    print(a==b)
->>>>>>> refs/remotes/origin/main
